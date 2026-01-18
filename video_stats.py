eac288ea9a442f076af3def4fa9d20c2dfabe8f7
Pramila Tamang <theeng.pramila96@gmail.com>
2026-01-18 16:04:26 +1300

    rendered the detail of video id

--- video_stats.py
+++ video_stats.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path=".env")
 
 API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = "mrnigelng"
+CHANNEL_HANDLE = "NepaHype"
 maxResults = 1
 
 
@@ -58,9 +58,47 @@ def get_video_ids(paylistId):
 
 
 
+def extract_video_data(video_ids):
+    extracted_data = []
+
+    def batch_list(video_id_list, batch_size):
+        for video_id in range(0, len(video_id_list), batch_size):
+            yield video_id_list[video_id: video_id + batch_size]
+
+    
+       
+    try:
+         for batch in batch_list(video_ids, maxResults):
+            video_ids_str = ",". join(batch)
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}"
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get('items',[]):
+                video_id = item['id']
+                snippet = item['snippet']
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+
+                video_data = {
+                    "video_id": video_id,
+                    "title":snippet['title'],
+                    "publishedAt":snippet['publishedAt'],
+                    "duration":contentDetails['duration'],
+                    "viewCount":statistics.get('viewCount',None),
+                    "likeCount":statistics.get('likeCount', None),
+                    "commentCount":statistics.get('commentCount', None),
+                }    
+                extracted_data.append(video_data)
+            return extracted_data    
+    except requests.requestsException as e:
+        raise e
+
 
 if __name__=="__main__":
 
     playlistId = get_paylist_id()
     video_ids = get_video_ids(playlistId)
-    print(video_ids)    
+    print(extract_video_data(video_ids))
+  
